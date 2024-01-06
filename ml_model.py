796691eae88a10f898ec796f80bfe87111439b94
maroofsayed <maroofsayed98@gmail.com>
2024-01-06 19:11:18 +0530

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("adding ML code here")
+
+def model1():
+    print("adding code for model 1")
+
+def model2():
+    print("adding code for model 2")
+
+def model3():
+    print("adding code for model 3")
